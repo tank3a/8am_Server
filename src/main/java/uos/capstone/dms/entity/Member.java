fd2f5bc1379d3fcab02d971b0494cffaf2f14b28
tank3a <tank3a@gmail.com>
2023-02-03 11:12:38 +0900

    Set Up Server

---
+++ src/main/java/uos/capstone/dms/entity/Member.java
@@ -0,0 +1,7 @@
+package uos.capstone.dms.entity;
+
+import lombok.Builder;
+
+@Builder
+public class Member {
+}
---
+++ src/test/java/uos/capstone/dms/DmsApplicationTests.java
@@ -0,0 +1,9 @@
+package uos.capstone.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsApplicationTests {
+
+}
