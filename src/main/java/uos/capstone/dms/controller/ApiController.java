bdbe4478ce2a3d76708991722b68fac9c5607b24
tank3a <tank3a@gmail.com>
2023-02-11 00:12:49 +0900

    FEATURE: Apply swagger-ui

--- src/main/java/uos/capstone/dms/controller/ApiController.java
+++ src/main/java/uos/capstone/dms/controller/ApiController.java
@@ -1,5 +1,6 @@
 package uos.capstone.dms.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uos.capstone.dms.domain.security.TokenDTO;
 import uos.capstone.dms.service.AuthService;
 
-@Controller
+@RestController
 @Log4j2
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class ApiController {
 
     private final AuthService authService;
 
+    @Operation(summary = "토큰 갱신")
     @PostMapping("/refreshToken")
     public ResponseEntity<TokenDTO> refreshToken(@RequestBody TokenDTO tokenDTO) {
         return ResponseEntity.ok(authService.refresh(tokenDTO));
--- src/main/java/uos/capstone/dms/controller/MemberController.java
+++ src/main/java/uos/capstone/dms/controller/MemberController.java
@@ -1,14 +1,13 @@
 package uos.capstone.dms.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import uos.capstone.dms.domain.security.TokenDTO;
 import uos.capstone.dms.domain.user.LoginRequestDTO;
 import uos.capstone.dms.domain.user.MemberRequestDTO;
@@ -16,7 +15,8 @@ import uos.capstone.dms.domain.user.MemberResponseDTO;
 import uos.capstone.dms.security.SecurityUtil;
 import uos.capstone.dms.service.MemberService;
 
-@Controller
+@Tag(name = "사용자 관련", description = "사용자 관련 API")
+@RestController
 @Log4j2
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -25,6 +25,7 @@ public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/signup")
     public String memberSignup(@RequestBody MemberRequestDTO memberRequestDTO) {
 
@@ -33,11 +34,13 @@ public class MemberController {
         return "redirect:/user/login";
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> memberLogin(@RequestBody LoginRequestDTO loginRequestDTO) {
         return ResponseEntity.ok(memberService.login(loginRequestDTO));
     }
 
+    @Operation(summary = "회원정보 호출")
     @GetMapping("/getMemberData")
     public ResponseEntity<MemberResponseDTO> authenticate() {
         return ResponseEntity.ok(memberService.findMemberByUserId(SecurityUtil.getCurrentUsername()));
